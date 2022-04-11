30727b652118921af76d9e031bb278aece376c66
Tharindu <tharindu@pointgreysolutions.com>
2022-04-11 14:50:52 +0530

    payload test

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,22 @@
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
+
 const Login = () => {
+
+    const dispatch = useDispatch()
+
     return ( 
         <div>
             <h3>Login</h3>
-            <button>Loog</button>
+            <button onClick={()=>{dispatch(
+                login(
+                    {
+                        name:"Sandeepa",
+                        age:20,
+                        email:"sandeepa@email.com"
+                    }
+                )
+            )}}>Login</button>
         </div>
      );
 }
